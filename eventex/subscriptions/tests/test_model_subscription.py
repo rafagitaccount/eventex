4f2a914b3004572333d2566a15822c4605c283c1
Rafael R Oliveira <consultoria@tekplus.com.br>
2021-06-16 14:50:00 -0300

    Add field paid

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -24,20 +24,6 @@ class SubscriptionModelTest(TestCase):
     def test_str(self):
         self.assertEqual('Henrique Bastos', str(self.obj))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def test_paid_default_to_false(self):
+        """ By default paid must be false """
+        self.assertEqual(False, self.obj.paid)
