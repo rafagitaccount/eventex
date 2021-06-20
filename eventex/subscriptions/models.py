0b4a595d02f071a0f560a61932a36c2a3b1f6376
Rafael R Oliveira <consultoria@tekplus.com.br>
2021-06-20 10:29:20 -0300

    Implements modelform

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -1,11 +1,12 @@
+from eventex.subscriptions.validators import validate_cpf
 from django.db import models
 
 
 class Subscription(models.Model):
     name = models.CharField('nome', max_length=100)
-    cpf = models.CharField('cpf', max_length=11)
-    email = models.EmailField('e-mail')
-    phone = models.CharField('telefone', max_length=20)
+    cpf = models.CharField('cpf', max_length=11, validators=[validate_cpf])
+    email = models.EmailField('e-mail', blank=True)
+    phone = models.CharField('telefone', blank=True, max_length=20)
     created_at = models.DateTimeField('criado em', auto_now=True)
     paid = models.BooleanField('pago', default=False)
 
